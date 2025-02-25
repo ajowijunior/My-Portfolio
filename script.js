c64b04e301623b9e34f962f263860e7770bef7f8
Ajowi Oyugi <ajowijunior2021@gmail.com>
2025-02-26 00:51:15 +0300

    made changes in html css and javascript

---
+++ script.js
@@ -0,0 +1,37 @@
+
+//for typewriter effect
+const texts = [
+    "Software Developer",
+    "YouTuber",
+    "Web Designer"
+]
+
+let speed = 100;
+const textElements = document.querySelector(".typewriter-text");
+
+let textIndex = 0;
+let characterIndex = 0;
+
+function typeWriter() {
+    if (characterIndex < texts[textIndex].length) {
+        textElements.innerHTML += texts[textIndex].charAt(characterIndex);
+        characterIndex++;
+        setTimeout(typeWriter, speed);
+    } else {
+        setTimeout(eraseText, 1000); // Delay before erasing
+    }
+}
+
+function eraseText() {
+    if (textElements.innerHTML.length > 0) {
+        textElements.innerHTML = textElements.innerHTML.slice(0, -1);
+        setTimeout(eraseText, 50); // Speed of erasing
+    } else {
+        textIndex = (textIndex + 1) % texts.length; // Corrected reference to texts array
+        characterIndex = 0;
+        setTimeout(typeWriter, 500); // Delay before typing the next text
+    }
+}
+
+// Start the typing effect
+typeWriter();
